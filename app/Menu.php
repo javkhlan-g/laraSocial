7b0be3a9caa6f527517311b2989946ef6e07b2ed
javkhlan.g <javhaaintus@yahoo.com>
2017-03-31 00:54:21 +0800

    updated

---
+++ app/Menu.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Social;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Menu extends Model
+{
+    //
+}
---
+++ resources/views/account.blade.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: javkhlan
+ * Date: 3/25/2017
+ * Time: 12:22 AM
+ */
